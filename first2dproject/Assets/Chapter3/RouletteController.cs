2e031af4a261dc1f4aedd9282ef1faa2b1911e93
SuYeon-Hwnag <109446753+SuYeon-Hwnag@users.noreply.github.com>
2022-08-31 23:30:34 +0900

    0831

--- first2dproject/Assets/Chapter3/RouletteController.cs
+++ first2dproject/Assets/Chapter3/RouletteController.cs
@@ -1,23 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RouletteController : MonoBehaviour
 {
     float rotSpeed = 0; // 회전 속도
     bool end = false;
+    int carmovecount = 0; // 룰렛 돌릴 기회
+    GameObject count;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        count = GameObject.Find("count");
     }
 
     // Update is called once per frame
     void Update()
     {
         // 클릭하면 회전한다.
-        if (Input.GetMouseButtonDown(0))
+        // Input.GetMouseButtonDown(0) -> 한 번만 클릭
+        // Input.GetMouseButton(0) -> 쭉 클릭
+
+        if (Input.GetMouseButton(0))
         {
             rotSpeed = 5;
             end = true;
@@ -38,26 +45,38 @@ public class RouletteController : MonoBehaviour
 
                 {
                     Debug.Log("운수 나쁨");
+                    count.GetComponent<Text>().text = "남은 횟수: 1";
+                    carmovecount = 1;
                 }
                 else if (gameObject.transform.eulerAngles.z > 30 && gameObject.transform.eulerAngles.z <= 90)
                 {
                     Debug.Log("운수 대통");
+                    count.GetComponent<Text>().text = "남은 횟수: 2";
+                    carmovecount = 2;
                 }
                 else if (gameObject.transform.eulerAngles.z > 90 && gameObject.transform.eulerAngles.z <= 150)
                 {
                     Debug.Log("운수 매우 나쁨");
+                    count.GetComponent<Text>().text = "남은 횟수: 1";
+                    carmovecount = 1;
                 }
                 else if (gameObject.transform.eulerAngles.z > 150 && gameObject.transform.eulerAngles.z <= 210)
                 {
                     Debug.Log("운수 보통");
+                    count.GetComponent<Text>().text = "남은 횟수: 3";
+                    carmovecount = 3;
                 }
                 else if (gameObject.transform.eulerAngles.z > 210 && gameObject.transform.eulerAngles.z <= 270)
                 {
                     Debug.Log("운수 조심");
+                    count.GetComponent<Text>().text = "남은 횟수: 2";
+                    carmovecount = 2;
                 }
                 else
                 {
                     Debug.Log("운수 좋음");
+                    count.GetComponent<Text>().text = "남은 횟수: 4";
+                    carmovecount = 4;
                 }
                 end = false;
             }
